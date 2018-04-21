e48cf14173f5771dd0ea3d0f2a389d2d752210f0
shibodaev <shibodaev@intervolga.ru>
2018-04-22 00:52:44 +0300

    V0.0.3

--- js/common.js
+++ js/common.js
@@ -1,21 +1,30 @@
 let 	parentElement = document.body,
-		input = document.createElement("input");
-		arr = [input];
-/* цикл добавления елеентов на страницу */
-		for (let i = 0; i < arr.length; i++) {
-			parentElement.appendChild(arr[i]);
-		}
+		input = document.createElement("input"),
+		label = document.createElement("label"),
+		arr = [input, label];
 
-let attr = {
-	type: "text",
-	id: "input"
-};
-function setAttr( el, attr ) {
-	for (let key in attr){
-		el.setAttribute(key,attr[key]);
+/* Разобратся с добавлением автоматичиски атрибутов с id label */
+	label.setAttribute("for","input");
+	label.textContent = (' Добавьте Задание');
+
+/* цикл добавления елеентов на страницу */
+	for (let i = 0; i < arr.length; i++) {
+		parentElement.appendChild(arr[i]);
 	}
-}
-setAttr();
+/* добавление аттрибутов инпуту*/
+	let attr = {
+		type: "text",
+		id: "input"
+	};
+		function setAttr( el, attr ) {
+			for (let key in attr) {
+				el.setAttribute(key,attr[key]);
+			}
+		}
+	setAttr(input, attr);
+
+
+
 
 
 
