34133d035c362a02f677defce555d16b0d971969
shibodaev <shibodaev@intervolga.ru>
2018-04-22 02:15:56 +0300

    Разобратся с инпутами

--- js/common.js
+++ js/common.js
@@ -1,27 +1,46 @@
-let 	parentElement = document.body,
-		input = document.createElement("input"),
-		label = document.createElement("label"),
-		arr = [input, label];
-
-/* Разобратся с добавлением автоматичиски атрибутов с id label */
-	label.setAttribute("for","input");
-	label.textContent = (' Добавьте Задание');
-
-/* цикл добавления елеентов на страницу */
-	for (let i = 0; i < arr.length; i++) {
-		parentElement.appendChild(arr[i]);
+let parentElement = document.body,
+	input = document.createElement("input"),
+	label = document.createElement("label"),
+	ol = document.createElement("ol"),
+	inputSubmit = document.createElement("input"),
+	idInput = input.value,
+	arr = [input, label, ol, inputSubmit],
+	inputVal = [];
+/* 1 ------------------------------- */
+/* цикл добавления елементов на страницу */
+for (let i = 0; i < arr.length; i++) {
+	parentElement.appendChild(arr[i]);
+}
+/* 2 ------------------------------- */
+/* добавление аттрибутов инпуту */
+function setAttr(el, button) {
+	button = { type: "submit", id: "button", value: "Добавить", onclick: "add()" };
+	for (let key in button) {
+		el.setAttribute(key, button[key]);
 	}
-/* добавление аттрибутов инпуту*/
-	let attr = {
-		type: "text",
-		id: "input"
-	};
-		function setAttr( el, attr ) {
-			for (let key in attr) {
-				el.setAttribute(key,attr[key]);
-			}
-		}
-	setAttr(input, attr);
+	console.log('0');
+}
+setAttr(input);
+parentElement
+function setAttr1(el, attr) {
+	attr = { type: "text", id: "input"};
+	for (let key in attr) {el.setAttribute(key, attr[key])}
+	console.log('1');
+}
+setAttr1(input);
+/* 4 ------------------------------- */
+/* создаю функцию получения значения инпут */
+// function save() {
+// 	addVal = inputVal.push(idInput);
+// 	return addVal;
+// }
+
+/* 3 ------------------------------- */
+/* Разобратся с добавлением автоматичиски атрибутов с id label */
+label.setAttribute("for", "input");
+label.textContent = (' Добавьте Задание');
+
+
 
 
 
