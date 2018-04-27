c6a14fa82660d713e83ac01ab761961d5d84cd00
shibodaev <mike756@mail.ru>
2018-04-27 07:19:02 +0300

    v0.0.5

--- js/common.js
+++ js/common.js
@@ -1,55 +1,74 @@
-let parentElement = document.body,
-	input          = document.createElement("input"),
-	label          = document.createElement("label"),
-	ol             = document.createElement("ol"),
-	inputSubmit    = document.createElement("input"),
-	inputSubmit2   = document.createElement("input"),
-	arr            = [ input, label, ol, inputSubmit, inputSubmit2 ],
-	arrOl          = [],
-/* получение di */
-	idText         = document.getElementById('input'),
-	idSubmit       = document.getElementById('button'),
-	checkbox       = document.querySelector('input[type="checkbox"]'),
-	idOl 				= document.getElementById('list');
+var parentElement = document.body,
+	input = document.createElement("input"),
+	label = document.createElement("label"),
+	ol = document.createElement("ol"),
+	inputSubmit = document.createElement("input"),
+	span = document.createElement("span"),
+	inputSubmit2 = document.createElement("input"),
+	arr = [input, label, ol, inputSubmit, inputSubmit2],
+	arrOl = [],
+/* получение id */
+	idText = document.getElementById('input'),
+	idSubmit = document.getElementById('button'),
+	idOl = document.getElementById('list');
 
 
 /* 4 ------------------------------- */
 /* создаю функцию получения значения инпут */
+
 function add() {
 	var idInput = input.value,
-	li          = document.createElement("li");
+		li = document.createElement("li");
 	if (idInput == "") {
 		arrOl = [];
 		return ol;
 	} else {
 		arrOl.push(idInput);
 		ol.appendChild(li);
-		li.innerHTML = '<input type="checkbox"  >' + idInput;
+		li.innerHTML = '<input type="checkbox">' + '<span class="mission">' + idInput + '</span>';
+		var checkBox = document.querySelector('[type="checkbox"]');
+		var mission = document.querySelector('.mission');
+		checkBox.addEventListener('click', checket);
+		function checket() {
+			if (checkBox) {
+				return mission.classList.add("mission-none");
+			} else {
+				return mission.classList.remove("mission-none");
+			}
+		}
 	}
 }
-// function liRedactor(el) {
-//
-// }
+function check() {
+	if (checkbox == true) {
+
+	} else {
+		console.log(false);
+	}
+}
+
+
 function del() {
 	arrOl.pop();
 	ol.removeChild(ol.children[0]);
 }
 /* 1 ------------------------------- */
 /* цикл добавления елементов на страницу */
-for (let i = 0; i < arr.length; i++) {
+for (var i = 0; i < arr.length; i++) {
 	parentElement.appendChild(arr[i]);
 }
 /* 2 ------------------------------- */
 /* добавление аттрибутов инпуту */
 function setAttr1(el, attr) {
-	attr = { type: "text", id: "input"};
-	for (let key in attr) {el.setAttribute(key, attr[key])}
+	attr = { type: "text", id: "input" };
+	for (var key in attr) {
+		el.setAttribute(key, attr[key])
+	}
 }
 setAttr1(input);
 
 function setAttr(el, button) {
 	button = { type: "submit", id: "button", value: "Добавить", onclick: "add()" };
-	for (let key in button) {
+	for (var key in button) {
 		el.setAttribute(key, button[key]);
 	}
 }
@@ -57,7 +76,7 @@ setAttr(inputSubmit);
 
 function button2(el, buttonAttr) {
 	buttonAttr = { type: "submit", id: "button", value: "Удалить", onclick: "del()" };
-	for (let key in buttonAttr) {
+	for (var key in buttonAttr) {
 		el.setAttribute(key, buttonAttr[key]);
 	}
 }
