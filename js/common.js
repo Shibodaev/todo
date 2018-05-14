0a203daf79f2e33724d90b1e481e77feac59fc91
shibodaev <mike756@mail.ru>
2018-05-14 06:33:53 +0300

    пишу todo c урока

--- js/common.js
+++ js/common.js
@@ -1,121 +1,97 @@
-var parentElement = document.body,
-	input = document.createElement("input"),
-	label = document.createElement("label"),
-	ol = document.createElement("ol"),
-	inputSubmit = document.createElement("input"),
-	span = document.createElement("span"),
-	inputSubmit2 = document.createElement("input"),
-	arr = [input, label, ol, inputSubmit, inputSubmit2],
-	arrOl = [],
-/* получение id */
-	idText = document.getElementById('input'),
-	idSubmit = document.getElementById('button'),
-	idOl = document.getElementById('list');
 
 
-/* 4 ------------------------------- */
-/* создаю функцию получения значения инпут */
 
-function add() {
-	var idInput = input.value,
-		li = document.createElement("li");
-	if (idInput == "") {
-		arrOl = [];
-		return ol;
-	} else {
-		arrOl.push(idInput);
-		ol.appendChild(li);
-		li.innerHTML = '<input type="checkbox">' + '<span class="mission">' + idInput + '</span>';
-		var checkBox = document.querySelector('[type="checkbox"]');
 
-		checkBox.onclick = function () {
-			arrOl.forEach(function () {
-				return this.nextSibling.classList.add("mission-none");
-			});
-			// if(checkBox) {
-			// 	arrOl[i] = this.value;
-			// 	console.log(arrOl);
-			// 	// i++;
-			// 	// event.currentTarget.nextSibling.classList.add("mission-none");
-			// }
-		};
-		// // mission.classList.remove("mission-none");
-		// if () {
-		//
-		//
-		// } else {
-		// 	return mission.classList.remove("mission-none");
-		// }
-		// checkBox.addEventListener('click', checket);
-		// function checket() {
-		// 	if (checkBox.checked) {
-		// 		for(var i = 0; i < ol.children.length; i++) {
-		// 			mission.classList.add("mission-none");
-		// 		}
-		//
-		// 	} else {
-		// 		return mission.classList.remove("mission-none");
-		// 	}
-		// }
-	}
-}
-// function check() {
-// 	if (checkbox == true) {
+// var parentElement = document.body,
+// 	input = document.createElement("input"),
+// 	label = document.createElement("label"),
+// 	ol = document.createElement("ol"),
+// 	inputSubmit = document.createElement("input"),
+// 	span = document.createElement("span"),
+// 	inputSubmit2 = document.createElement("input"),
+// 	arr = [input, label, ol, inputSubmit, inputSubmit2],
+// 	arrOl = [],
+// /* получение id */
+// 	idText = document.getElementById('input'),
+// 	idSubmit = document.getElementById('button'),
+// 	idOl = document.getElementById('list');
 //
+//
+// /* 4 ------------------------------- */
+// /* создаю функцию получения значения инпут */
+//
+// function add() {
+// 	var idInput = input.value,
+// 		li = document.createElement("li");
+// 	if (idInput == "") {
+// 		arrOl = [];
+// 		return ol;
 // 	} else {
-// 		console.log(false);
+// 		arrOl.push(idInput);
+// 		ol.appendChild(li);
+// 		li.innerHTML = '<input type="checkbox">' + '<span class="mission">' + idInput + '</span>';
+//		
+// 		li.onclick = function (e) {
+// 			var checkBox = document.querySelector('[type="checkbox"]');
+// 			var mission = document.querySelector('.mission');
+// 			if (checkBox.checked) {
+// 				return e.target.nextSibling.classList.add("mission-none")
+// 			} else {
+// 				return e.target.getElementsByClassName("mission").classList.remove("mission-none")
+// 			}
+// 		}
 // 	}
 // }
-
-
-function del() {
-	arrOl.pop();
-	ol.removeChild(ol.children[0]);
-}
-/* 1 ------------------------------- */
-/* цикл добавления елементов на страницу */
-for (var i = 0; i < arr.length; i++) {
-	parentElement.appendChild(arr[i]);
-}
-/* 2 ------------------------------- */
-/* добавление аттрибутов инпуту */
-function setAttr1(el, attr) {
-	attr = { type: "text", id: "input" };
-	for (var key in attr) {
-		el.setAttribute(key, attr[key])
-	}
-}
-setAttr1(input);
-
-function setAttr(el, button) {
-	button = { type: "submit", id: "button", value: "Добавить", onclick: "add()" };
-	for (var key in button) {
-		el.setAttribute(key, button[key]);
-	}
-}
-setAttr(inputSubmit);
-
-function button2(el, buttonAttr) {
-	buttonAttr = { type: "submit", id: "button", value: "Удалить", onclick: "del()" };
-	for (var key in buttonAttr) {
-		el.setAttribute(key, buttonAttr[key]);
-	}
-}
-button2(inputSubmit2);
-
-/* 3 ------------------------------- */
-/* Разобратся с добавлением автоматичиски атрибутов с id label */
-label.setAttribute("for", "input");
-label.textContent = (' Добавьте Задание');
-ol.setAttribute("id", "list");
-
-
-
-
-
-
-
-
-
-
-
+//
+//
+// function del() {
+// 	arrOl.pop();
+// 	ol.removeChild(ol.children[0]);
+// }
+// /* 1 ------------------------------- */
+// /* цикл добавления елементов на страницу */
+// for (var i = 0; i < arr.length; i++) {
+// 	parentElement.appendChild(arr[i]);
+// }
+// /* 2 ------------------------------- */
+// /* добавление аттрибутов инпуту */
+// function setAttr1(el, attr) {
+// 	attr = { type: "text", id: "input" };
+// 	for (var key in attr) {
+// 		el.setAttribute(key, attr[key])
+// 	}
+// }
+// setAttr1(input);
+//
+// function setAttr(el, button) {
+// 	button = { type: "submit", id: "button", value: "Добавить", onclick: "add()" };
+// 	for (var key in button) {
+// 		el.setAttribute(key, button[key]);
+// 	}
+// }
+// setAttr(inputSubmit);
+//
+// function button2(el, buttonAttr) {
+// 	buttonAttr = { type: "submit", id: "button", value: "Удалить", onclick: "del()" };
+// 	for (var key in buttonAttr) {
+// 		el.setAttribute(key, buttonAttr[key]);
+// 	}
+// }
+// button2(inputSubmit2);
+//
+// /* 3 ------------------------------- */
+// /* Разобратся с добавлением автоматичиски атрибутов с id label */
+// label.setAttribute("for", "input");
+// label.textContent = (' Добавьте Задание');
+// ol.setAttribute("id", "list");
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
