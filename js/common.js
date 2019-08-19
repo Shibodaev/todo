c7b25ca95e9a23be97cdf3fb5e78bc735c83d081
Shibodaev <mike756@mail.ru>
2019-08-19 06:48:49 +0400

    last version

--- js/common.js
+++ js/common.js
@@ -1,37 +1,39 @@
 
-'use strict'
-; (function () {
-	var entryField = document.getElementById('enter'),
-		addButton = document.getElementById('button'),
-		list = document.getElementById('list'),
-		date = new Date(),storage = localStorage;
+
+
+
+// ; (function () {
+// 	var entryField = document.getElementById('enter'),
+// 		addButton = document.getElementById('button'),
+// 		list = document.getElementById('list'),
+// 		date = new Date(),storage = localStorage;
 		
-	var inputValue = () => {
-		if(entryField.value.length > 0) {
-			addButton.removeAttribute('disabled');
-		} else {
-			addButton.setAttribute('disabled', 'disabled');
-		}
-	}
+// 	var inputValue = () => {
+// 		if(entryField.value.length > 0) {
+// 			addButton.removeAttribute('disabled');
+// 		} else {
+// 			addButton.setAttribute('disabled', 'disabled');
+// 		}
+// 	}
 	
-	entryField.addEventListener("input",inputValue);
+// 	entryField.addEventListener("input",inputValue);
 	
-	var addCase = addButton.addEventListener("click", function () {
-		var listItem = document.createElement('li'),
-		close = document.createElement("span");
-		close.classList.add('close');
+// 	var addCase = addButton.addEventListener("click", function () {
+// 		var listItem = document.createElement('li'),
+// 		close = document.createElement("span");
+// 		close.classList.add('close');
 		
-		list.appendChild(listItem).innerHTML = 
-		`<span class="text">${entryField.value}</span>
-		<span class="time"> ${date.toLocaleDateString()}</span>`
-		listItem.appendChild(close).innerHTML= "X";
-		close.onclick =()=>close.parentNode.remove();
-		listItem.onclick = () => listItem.firstChild.style.textDecoration =
-	"line-Through";
-	console.log(list.firstChild);
-	}, true);
-	function storage(key){
-	  localStorage.setItem(key, JSON.stringify(data));
-	}
+// 		list.appendChild(listItem).innerHTML = 
+// 		`<span class="text">${entryField.value}</span>
+// 		<span class="time"> ${date.toLocaleDateString()}</span>`
+// 		listItem.appendChild(close).innerHTML= "X";
+// 		close.onclick =()=>close.parentNode.remove();
+// 		listItem.onclick = () => listItem.firstChild.style.textDecoration =
+// 	"line-Through";
+// 	console.log(list.firstChild);
+// 	}, true);
+// 	function storage(key){
+// 	  localStorage.setItem(key, JSON.stringify(data));
+// 	}
 	
-})();
+// })();
